85c8a66f6b8e5b508b54af75bbe7c15d79c42300
Daria Shcheglova <ellerden@gmail.com>
2017-06-17 13:18:41 +0300

    subscriptions and comments added, needs refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
 
   devise_for :users
   root "events#index"
-  resources :events
+
   resources :users, only: [:show, :edit, :update]
+
+  resources :events do
+    resources :comments, only: [:create, :destroy]
+    resources :subscriptions, only: [:create, :destroy]
+  end
+
 
 end
